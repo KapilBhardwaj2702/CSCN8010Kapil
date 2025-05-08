6a221092defa384a561ac4d69caf47d78ce52369
kapilbhardwaj2702 <kapubhardwaj1@gmail.com>
2025-05-08 14:24:26 -0400

    Add HelloWorld.py with HelloKapil class

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+class HelloKapil:
+    def greet(self):
+        print("Hello Kapil to Python World")
+        # ...existing code...
+if __name__ == "__main__":
+    hello = HelloKapil()
+    hello.greet()
