0b69d7d80718fea680ec8d916c7e4b907fc02305
KapilBhardwaj2702 <kapubhardwaj1@gmail.com>
2025-06-06 08:25:55 -0400

    Delete HelloWorld.py

--- HelloWorld.py
+++
@@ -1,7 +0,0 @@
-class HelloKapil:
-    def greet(self):
-        print("Hello Kapil to Python World")
-        # ...existing code...
-if __name__ == "__main__":
-    hello = HelloKapil()
-    hello.greet()
